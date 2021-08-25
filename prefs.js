17a9b51cdbd8f37ebe00ea7294120f317e227503
Hum Ler <hum.ler@outlook.com>
2021-08-25 14:20:57 +0800

    Add preferences window

---
+++ prefs.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const { Gdk, Gio, Gtk } = imports.gi;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+
+const cssProvider = new Gtk.CssProvider();
+cssProvider.load_from_file(Me.dir.get_child("stylesheet.css"));
+Gtk.StyleContext.add_provider_for_display(
+  Gdk.Display.get_default(),
+  cssProvider,
+  Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+);
+
+const settings = ExtensionUtils.getSettings(
+  'org.gnome.shell.extensions.' + Me.metadata.uuid
+);
+
+function init() { }
+
+function buildPrefsWidget() {
+  let builder = new Gtk.Builder();
+  builder.add_from_file(Me.dir.get_child("prefs.ui").get_path());
+
+  settings.bind(
+    "show-close-button",
+    builder.get_object("showCloseButtonSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+  settings.bind(
+    "show-move-to-workspace-left-button",
+    builder.get_object("showMoveToWorkspaceLeftButtonSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+  settings.bind(
+    "show-move-to-workspace-right-button",
+    builder.get_object("showMoveToWorkspaceRightButtonSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+  settings.bind(
+    "show-always-on-top-toggle",
+    builder.get_object("showAlwaysOnTopToggleSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+  settings.bind(
+    "show-always-on-visible-workspace-toggle",
+    builder.get_object("showAlwaysOnVisibleWorkspaceToggleSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+  settings.bind(
+    "monitor-current-focus-window",
+    builder.get_object("monitorCurrentFocusWindowSwitch"),
+    "active",
+    Gio.SettingsBindFlags.DEFAULT
+  );
+
+  return builder.get_object("prefsWidget");
+}
